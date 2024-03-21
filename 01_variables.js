cdd5875ec30b66aec7d5e7e16da711cc07b1a59f
Satyapalbsinghe <satyapalbsinghu@gmail.com>
2024-03-21 15:09:26 +0530

    data

---
+++ 01_variables.js
@@ -0,0 +1,23 @@
+const accountID = "007"
+let accountEmail = "test@gamil.com"
+var accountPassword = "123"
+accountCity = "Mohali"
+let accountState;
+// accountID = "20" Not alowed, because we can not chnage the const variables
+
+// Assignment to constant variable.
+
+
+// console.log(accountID);
+
+accountEmail = "vishal@gamil.com"
+accountPassword = "789"
+accountCity = "Chandighar"
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+
+
+/*
+prefer not to use var
+beacuse of issue in block scope and finctional scope
+*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //Treat all JS code as newer version
+// alert(3+3) // It is not working here as we are using nodejs, not browser
+
+console.log(3+3)
+console.log("vishal") //code readability should be high
+
+
+
+//number => 2 to power 53(range)
+//bigint => to store the Big range of data 
+//string => ""
+//boolean =>true/false
+//null => standalone value
+//underdeined => 
+//symbol => unique
+//object
+
+console.log(typeof undefined); //Output => underfined  
+console.log(typeof null); // output => object
